f697c6338c6e5872a21149403bf23332ec1fd9c2
Eric Lorite Palomino <ericlorpal@gmail.com>
2023-08-18 13:53:18 +0200

    Add routes for the basic controllers (#6)

--- main.go
+++ main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"loridev/go-todo-app/config"
+	"loridev/go-todo-app/routes"
 
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
+func init() {
 	config.LoadEnvVariables()
 	config.ConnectToDB()
+}
+
+func main() {
 	defer config.CloseDB()
 
 	r := gin.Default()
+
+	routes.TodosRouter(r)
+
 	config.RunServer(r)
 }
---
+++ types/rest_error.go
@@ -0,0 +1,6 @@
+package types
+
+type RestError struct {
+	Status uint
+	Error string
+}
